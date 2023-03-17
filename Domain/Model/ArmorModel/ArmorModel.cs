98d5e59592e19a498eeaa6ab5383b2a107183116
giorgi abashidze <gabashidze99@gmail.com>
2023-03-17 23:59:28 +0400

    DefencePower property renamed to Defence. add new property: List<ZoneModel> Zone.

--- Domain/Model/ArmorModel/ArmorModel.cs
+++ Domain/Model/ArmorModel/ArmorModel.cs
@@ -1,10 +1,13 @@
-﻿namespace Domain.Models
+﻿using Domain.Model.HitPointModel;
+
+namespace Domain.Models
 {
     public class ArmorModel : AmmunitionModel
     {
-        public double DefensePower { get; set; }
-        public double Penetration { get; set; }
-        public double Luck { get; set; }
+        public double Defence { get; set; }
+        public double Penetration { get; set; } //определяет, насколько процентов может отказать защита.
+        public double Luck { get; set; } // удача 
+        public List<ZoneModel> Zone { get; set; } = new List<ZoneModel>(); // определяет, какую зону тела защищает
     }
 }
 
