acf94615827cfe516a7d28005abb9937a3605817
giorgi abashidze <gabashidze99@gmail.com>
2023-03-18 00:28:44 +0400

    Create Goblin Character

--- Domain/Character/Goblin.cs
+++ Domain/Character/Goblin.cs
@@ -1,21 +1,24 @@
 ﻿using Domain.Models;
 using Domain.Service;
+using Domain.Model.Character;
 
 namespace Domain.Character
 {
     public class Goblin : CharacterModel
     {
+
         public override int Id { get => base.Id; set => base.Id = value; }
-        public override string Name { get => base.Name; set => base.Name = value; }
         public override double Life { get => base.Life; set => base.Life = value; }
+        public override string Name { get => base.Name; set => base.Name = value; }
         public override double Attack { get => base.Attack; set => base.Attack = value; }
         public override double Defence { get => base.Defence; set => base.Defence = value; }
-        public override CharacterRaceEnum Character { get => base.Character; set => base.Character = value; }
         public override ShieldModel Shield { get => base.Shield; set => base.Shield = value; }
-        public override List<WeaponModel> Weapon { get => base.Weapon; set => base.Weapon = value; }
+        public override EnemyLevelModel Level { get => base.Level; set => base.Level = value; }
         public override List<ArmorModel> Armor { get => base.Armor; set => base.Armor = value; }
+        public override List<WeaponModel> Weapon { get => base.Weapon; set => base.Weapon = value; }
         public override string ImageAddress { get => base.ImageAddress; set => base.ImageAddress = value; }
-
+        public override CharacterRaceEnum Character { get => base.Character; set => base.Character = value; }
+        
         public Goblin()
         {
             Id = 2;
@@ -24,6 +27,7 @@ namespace Domain.Character
             Attack = 17;
             Defence = 16;
             Character = CharacterRaceEnum.Goblin;
+            Level = EnemyLevelModel.Level_1;
             Shield = new ShieldModel();
             Weapon = new List<WeaponModel>();
             Armor = new List<ArmorModel>();
