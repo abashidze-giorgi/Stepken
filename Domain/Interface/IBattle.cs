6fe8d76161f5bbd03711b023b6e3354202c9a1d0
giorgi abashidze <gabashidze99@gmail.com>
2023-03-17 23:57:34 +0400

    create IBattle interface and add GetHitPower, GetDefencePower, GetBattleResult, IsdefenderAlive methods.

--- Domain/Interface/IBattle.cs
+++ Domain/Interface/IBattle.cs
@@ -6,9 +6,9 @@ namespace Domain.Interface
     public interface IBattle
     {
         public int WhoStartBattle(); // get randomly 0 or 1 in stage start
-        public double GetHitPower(UnitModel atacker); // return demage power 
-        public double GetDefencePower(UnitModel defender); // return defence power
+        public double GetHitPower(CharacterModel atacker); // return demage power 
+        public double GetDefencePower(CharacterModel defender); // return defence power
         public double GetBattleResult(ZoneModel zone, double hitPower, double defencePower);
-        public bool IsDefenderAlive(UnitModel defender, double HitPower);
+        public bool IsDefenderAlive(CharacterModel defender, double HitPower);
     }
 }
