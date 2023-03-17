8ebff9df545b21a32c8fc126bdf158567adb3f15
giorgi abashidze <gabashidze99@gmail.com>
2023-03-18 00:31:37 +0400

    Create Enum class EnemyLevelModel

---
+++ Domain/Model/Character/EnemyLevelModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Model.Character
+{
+    public enum EnemyLevelModel
+    {
+        Level_1 = 1,
+        Level_2 = 2,
+        Level_3 = 3,
+        Level_4 = 4,
+        Level_5 = 5,
+        Level_6 = 6,
+        Level_7 = 7,
+        Level_8 = 8,
+        Level_9 = 9,
+        Level_10 = 10
+    }
+}
