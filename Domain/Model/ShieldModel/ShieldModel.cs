5876ef04d1ac1f15192c8b38f8657e440938a67f
giorgi abashidze <gabashidze99@gmail.com>
2023-03-18 00:00:40 +0400

    rename Protection - property to Defence. ADd nbew property: List<ZoneModel> Zone

--- Domain/Model/ShieldModel/ShieldModel.cs
+++ Domain/Model/ShieldModel/ShieldModel.cs
@@ -1,9 +1,12 @@
-﻿namespace Domain.Models
+﻿using Domain.Model.HitPointModel;
+
+namespace Domain.Models
 {
     public class ShieldModel : AmmunitionModel
     {
-        public double Protection { get; set; } // сила защиты
+        public double Defence { get; set; } // сила защиты
         public double BlockChance { get; set; } // процент шанса отражения удара.
         public double AttackPenalty { get; set; } // насколько щит влияет на силу атаки юнита
+        public List<ZoneModel> Zone { get; set; } = new List<ZoneModel>(); //лист зон защиты 
     }
 }
